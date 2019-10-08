f1eda68293af9df9bcb871ada5443133fe8d04f8
kineticmarginal <43065450+kineticmarginal@users.noreply.github.com>
2019-10-08 16:12:50 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,21 +9,34 @@ import (
 
 var (
 	seed = flag.Int("seed", 0, "seed for random generator. unix(now) be default")
+	start = flag.Int("start", 1, "start of interval for random generator")
+	end = flag.Int("end", 6, "end of interval for random generator")
+	n = flag.Int("n", 1, "num of output values")
+	norepeat = flag.Bool("norepeat", false, "no repeat for values in output values")
 )
 
 // Фукнция должна вернуть число из интервала [l,r]
-//func randInterval
-//	return 0
-//}
+func randInterval(l, r int) int{
+	return rand.Intn(r)+l
+}
 
 
 func main() {
 	flag.Parse()
 	if *seed == 0 {
-		rand.Seed(time.Now().Unix())
+		rand.Seed(time.Now().UnixNano())
 	} else {
 		rand.Seed(int64(*seed))
 	}
-	// Dice roll 1..6
-	fmt.Println(rand.Intn(6) + 1)
+	if *start > *end{
+		fmt.Println("ERROR")
+	} else if *start == 1 && *end == 6{
+		for i := 0; i < *n; i++{
+			fmt.Println(randInterval(1, 6))
+		}
+	} else {
+		for i := 0; i < *n; i++ {
+			fmt.Println(randInterval(*start+0, *end+1))
+		}
+	}
 }
